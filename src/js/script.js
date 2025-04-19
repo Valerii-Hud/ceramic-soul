4bc0ff4f2dfee7ee7ad21ec257f142f0d2c01f7f
Valerii Hud <support@valerii-hud.com>
2025-04-19 10:26:32 +0200

    Added HTML structure for activities section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
+//——————~ THIRD PARTY PACKAGES ~——————//
+import "/node_modules/purecss/build/grids-min.css";
+import "/node_modules/purecss/build/grids-responsive-min.css";
+
+//——————~ MY STYLES ~——————//
 import "../sass/style.scss";
