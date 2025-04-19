741e75cae7aaf5489244c812fa1002f498f6d8c4
Valerii Hud <support@valerii-hud.com>
2025-04-19 16:42:43 +0200

    Added responsive web design for activities section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,2 @@
-//——————~ THIRD PARTY PACKAGES ~——————//
-import "/node_modules/purecss/build/grids-min.css";
-import "/node_modules/purecss/build/grids-responsive-min.css";
-
 //——————~ MY STYLES ~——————//
 import "../sass/style.scss";
